391286038afca4095fee5f419c14831ad49389a4
rperez <rperez@prodesk>
2025-10-15 17:28:41 -0500

    Ajuste en secrets

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Hola Mundo desde Python-Flask en Docker!"
+    return "¡Hola Mundo desde Flask en Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
